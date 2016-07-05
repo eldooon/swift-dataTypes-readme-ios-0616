43f5fa930e05af55f54c7a480b2a588524c1e574
eldooon <eldonchan91@gmail.com>
2016-07-05 14:07:26 -0400

    done

--- Types.playground/Contents.swift
+++ Types.playground/Contents.swift
@@ -1,18 +1,20 @@
 let thirdPlanet = "Earth"
+var ninthPlanet : String
 
-var ninthPlanet: String
 ninthPlanet = "Pluto"
 
 let fifthPlanet: String = "Jupiter"
-// fifthPlanet is a constant of type ______
+// fifthPlanet is a constant of type string
 
-let sixthPlanet: String = "Saturn"
-// sixthPlanet is a constant of type ______
+// let sixthPlanet: String = "Saturn"
+// sixthPlanet is a constant of type string
 
 let seventhPlanet = "Uranus"
-// seventhPlanet is a constant of type ______
+// seventhPlanet is a constant of type string
 
 let numberOfCountriesInAfrica = 54
 
+let sixthPlanet = "Saturn"
 let numberOfMoonsOfSaturn = 62
+
 print("There are \(numberOfMoonsOfSaturn) moons orbiting \(sixthPlanet)")
